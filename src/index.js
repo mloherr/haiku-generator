985effbeb2aa7acf63b15b1e10b15081fd93bb35
mloherr <herraizmaria08@gmail.com>
2024-06-06 16:38:36 +0200

    Add conect time out for better renderization

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ async function getDBConnection() {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    connectTimeout: 30000,
   });
   connection.connect();
   return connection;
